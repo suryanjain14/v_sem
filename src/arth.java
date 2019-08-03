85e49df70ec616b62e0db2e52fc8e95d89aedd03
Suryan Jain <suryanjain14@gmail.com>
2019-08-03 18:21:00 +0530

    q2 completed

---
+++ src/arth.java
@@ -0,0 +1,12 @@
+public class arth {
+    public static void main(String[] args) {
+
+        int x1 = 7+3*6/2-1,x2 = 2%2+2*2-2/2,x3=(3*9*(3+(9*3/(3))));
+        System.out.println(x1);
+        System.out.println(x2);
+        System.out.println(x3);
+        //ans cal before  execution
+        int ax1=15,ax2=3,ax3=324;
+
+    }
+}
