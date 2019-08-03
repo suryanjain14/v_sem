16b85c7524938e66e378c8b2773abd34596361c9
Suryan Jain <suryanjain14@gmail.com>
2019-08-03 19:03:29 +0530

    q2 completed

---
+++ src/oddint.java
@@ -0,0 +1,13 @@
+public class oddint {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 15; i++) {
+            if ((i % 2) != 0) {
+
+                sum = sum + i;
+            }
+
+        }
+        System.out.println(sum);
+    }
+}
