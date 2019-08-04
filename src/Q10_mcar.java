ce3439f2621a3c2fe99a1b47af794b4d2287349c
suryanjain14 <suryanjain14>
2019-08-04 15:00:20 +0530

    conflicts: :)

---
+++ src/Q10_mcar.java
@@ -0,0 +1,22 @@
+public class Q10_mcar {
+
+    public static int fact(int no, int fac, int rec) {
+        rec++;
+        fac = fac * no;
+        if (no == 1) {
+            return rec,fac;//problem baad me dhekha jye ga
+        }
+        no--;
+        fact(no, fac, rec);
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int rec;//this variavle will store how many time the method was called
+        int no = 5, fac = 1;
+        int temp = fact(no, fac, rec);
+
+    }
+
+
+}
