de8d30a48a462bb59486ea388da1cc770b39e9ef
suryanjain14 <suryanjain14>
2019-08-04 12:10:10 +0530

    q2 completed

---
+++ src/Q8_obj.java
@@ -0,0 +1,10 @@
+public class Q8_obj {
+
+    int var = 45;
+
+    public static void main(String[] args) {
+        Q8_obj obj = new Q8_obj();
+
+        System.out.println("print using object: " + obj.var);
+    }
+}
