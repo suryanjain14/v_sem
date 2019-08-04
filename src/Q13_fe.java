fdf8c749fd75dd4e0949b2cdeceb638371886b04
suryanjain14 <suryanjain14>
2019-08-04 16:09:45 +0530

    conflicts: :)

---
+++ src/Q13_fe.java
@@ -0,0 +1,9 @@
+public class Q13_fe {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        for (int x : arr) {
+            System.out.println(x);
+        }
+    }
+}
