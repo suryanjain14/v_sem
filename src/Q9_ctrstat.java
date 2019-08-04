54cf01303516f48f2b78deff27651df575b49408
Suryan Jain <suryanjain14@gmail.com>
2019-08-04 12:39:10 +0530

    conflicts: :)

--- src/Q9_ctrstat.java
+++ src/Q9_ctrstat.java
@@ -1,7 +1,7 @@
 public class Q9_ctrstat {
 
     public static void main(String[] args) {
-        //loops
+        //LOOPS
         //for loop
         System.out.println("for");
         for (int i = 0; i < 5; i++) {
@@ -23,5 +23,34 @@ public class Q9_ctrstat {
             System.out.println("the no is: " + i);
             i++;
         } while (i < 5);
+
+        //DECISION MAKING STATEMENTS:
+
+        //if,if else, else
+        int a = 0, b = 3, c = 9;
+        if (a == 2) {
+            System.out.println("if");
+        } else if (a == 0) {
+            System.out.println("if else");
+        } else
+            System.out.println("else");
+
+        //BRANCHING STATEMENTS:
+        //break
+        for (i = 0; i < 5; i++) {
+            if (i == 2)
+                break;
+            System.out.println(" break no is " + i);
+        }
+        //continue
+        for (i = 0; i < 5; i++) {
+            if (i == 2)
+                continue;
+            System.out.println("continue no is " + i);
+        }
+
+
     }
 }
+
+
