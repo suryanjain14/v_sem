e78580bfdde97e452bda50b57eaff7495a9c862c
suryanjain14 <suryanjain14>
2019-08-11 15:21:03 +0530

    conflicts: :)

--- src/Q20selectionsort.java
+++ src/Q20selectionsort.java
@@ -1,10 +1,10 @@
 public class Q20selectionsort {
     public static void main(String[] args) {
         int min, loc;
-        int[] a = {3, 4, 6, 5, 2, 1};
-        for (int i = 0; i < 6; i++) {
+        int[] a = {2, 7, 6, 1, 3, 2, 3, 43, 12, 23, 56, 76, 87, 67, 867, 45, 34, 345, 23, 8, 9, 2, 3, 4};
+        for (int i = 0; i < 24; i++) {
             loc = i;
-            for (int j = i; j < 6; j++) {
+            for (int j = i; j < 24; j++) {
                 if (a[j] < a[loc]) {
 
                     loc = j;
