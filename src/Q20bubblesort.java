2b8625c3f3c44a47c9f1295fe71a1f10009c1199
suryanjain14 <suryanjain14>
2019-08-11 15:31:52 +0530

    conflicts: :)

---
+++ src/Q20bubblesort.java
@@ -0,0 +1,17 @@
+public class Q20bubblesort {
+    public static void main(String[] args) {
+        int[] a = {3, 5, 1, 4, 2};
+        for (int i = 0; i < 5; i++) {
+            for (int j = i; j < 5; j++) {
+                if (a[i] > a[j]) {
+                    int temp = a[j];
+                    a[j] = a[i];
+                    a[i] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < 5; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
