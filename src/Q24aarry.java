7d92a51662b3f2b4a511f29e2e8e98253370411b
Suryan Jain <suryanjain14@gmail.com>
2019-09-02 21:36:20 +0530

    que 24 completed

--- src/Q24aarry.java
+++ src/Q24aarry.java
@@ -1,2 +1,17 @@
 public class Q24aarry {
+
+
+    static void sum(int[] a) {
+        int sum = 0;
+        for (int x : a) {
+            sum = x + sum;
+        }
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+
+        sum(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 12});
+
+    }
 }
