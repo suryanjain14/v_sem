bffd50965ec4eee5e3fead790c5e1b43d387bab8
Suryan Jain <suryanjain14@gmail.com>
2019-09-02 21:23:04 +0530

    conflicts: :)

--- src/Q23stack.java
+++ src/Q23stack.java
@@ -1,2 +1,28 @@
+import java.util.Stack;
+
 public class Q23stack {
+
+    static void pu(int a, Stack st) {
+        st.push(new Integer(a));
+        System.out.println(st.peek());
+    }
+
+    static void po(Stack st) {
+
+        st.pop();
+        System.out.println(st.peek());
+    }
+
+    public static void main(String[] args) {
+
+        Stack st = new Stack();
+        pu(27, st);
+        pu(31, st);
+        pu(42, st);
+        po(st);
+        po(st);
+        st.peek();
+    }
+
+
 }
---
+++ src/Q24aarry.java
@@ -0,0 +1,2 @@
+public class Q24aarry {
+}
