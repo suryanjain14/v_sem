31efc9ed803bd1f4e40d2bdceff1e62fb2bf0106
Suryan Jain <suryanjain14@gmail.com>
2019-09-02 23:07:40 +0530

    q29 completed

---
+++ src/Q30str.java
@@ -0,0 +1,10 @@
+public class Q30str {
+    public static void main(String[] args) {
+        String str = "asdhask aksjdh", str2 = "asdhask Aksjdh";
+
+        System.out.println(str.equals(str2));
+        System.out.println(str.equalsIgnoreCase(str2));
+
+    }
+
+}
