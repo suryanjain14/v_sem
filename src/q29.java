5338860158b0e2936c0953ed4e4a04523e681636
Suryan Jain <suryanjain14@gmail.com>
2019-09-02 22:50:08 +0530

    q29 completed

---
+++ src/q29.java
@@ -0,0 +1,17 @@
+public class q29 {
+
+    public static void main(String[] args) {
+        System.out.println(a.as);
+        for (a x : a.values()) {
+            System.out.println(x);
+        }
+    }
+
+    enum a {
+        as,
+        bs,
+        cs,
+        ds,
+        es,
+    }
+}
