e2c6f10b349e0ff6acdcee4152bf04e1d3d3022b
Suryan Jain <suryanjain14@gmail.com>
2019-10-30 14:47:38 +0530

    q29 completed

--- src/Q31str.java
+++ src/Q31str.java
@@ -1,14 +1,14 @@
 public class Q31str {
 
     public static void main(String[] args) {
-        String str = "daslkdas asdha ksd KAS";
+        String str = "daslkdaks askdha ksd KAS";
 
         int i = 0, n = 0, j = 0, no = 0;
 
         for (j = 0; j < str.length(); j++) {
             i = str.indexOf('k', n);
             if (i > n) {
-                n = i;
+                n = i + 1;
                 no++;
             }
 
