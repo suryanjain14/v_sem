a6dfb5f88725b3ac379d3fbb7b98d414f21b3fb9
Suryan Jain <suryanjain14@gmail.com>
2019-12-21 19:28:52 +0530

    Q51 completed all requirements done

---
+++ src/Q51_52Vehicle.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Q51_52Vehicle {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Vehicle {
+    String insuranceno, vehicleno, color;
+
+    void getConsumption() {
+    }
+
+    void displayConsuption() {
+    }
+}
+
+class TwoWheeler extends Vehicle {
+    String type, companey;
+    Scanner sc = new Scanner(System.in);
+
+    void maintainance() {
+    }
+
+    void average() {
+    }
+
+    void getType() {
+        type = sc.nextLine();
+
+    }
+
+    void getCompaney() {
+        companey = sc.nextLine();
+    }
+
+}
+
+class FourWheeler extends Vehicle {
+    void maintainance() {
+    }
+
+    void average() {
+    }
+
+}
+
+class Geared extends TwoWheeler {
+
+    void average() {
+    }
+}
+
+class NONGeared extends TwoWheeler {
+
+    void average() {
+    }
+}
