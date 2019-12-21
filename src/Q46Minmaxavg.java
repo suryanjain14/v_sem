bb44f7be4b8a13a19b0c40e3adda480b00884edd
Suryan Jain <suryanjain14@gmail.com>
2019-12-21 16:11:24 +0530

    q46 completed all requirements done

---
+++ src/Q46Minmaxavg.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Q46Minmaxavg {
+    public static void main(String[] args) {
+        int a, b, c;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter numbers a,b,c");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        int avg = (a + b + c) / 3;
+        int product = (a * b * c);
+        System.out.println("avg" + avg + "   product" + product);
+        int min = a < b && a < c ? a : ((b < c && b < a) ? b : c);
+        int max = a > b && a > c ? a : ((b > c && b > a) ? b : c);
+        System.out.println("min: " + min + " max: " + max);
+    }
+
+}
